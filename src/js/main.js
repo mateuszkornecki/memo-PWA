bbb4363e112a3fc0ce54a8a59fde1bbffaa19d94
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-08-09 10:45:06 +0200

    feat: added pair counter

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,8 @@ if ('serviceWorker' in navigator) {
 const parent = document.querySelector('.parent');
 let cardsId = [];
 let cardsValue = [];
-
+const displayPairCounter = document.querySelector('.pair-counter');
+let pairCounter = 0;
 parent.addEventListener('click', (e) => {
 
     if (cardsId.length < 2 && e.target.className === 'card') {
@@ -28,14 +29,16 @@ parent.addEventListener('click', (e) => {
         card.classList.add('card--reverse');
         cardsId.push(id);
         cardsValue.push(card.innerHTML);
+
+        //If pairs were found
         if (cardsValue[0] === cardsValue[1]) {
-            console.log('PARA')
+            pairCounter++;
+            displayPairCounter.innerHTML = `Znaleziono ${pairCounter} par!`
             cardsId.forEach(element => {
                 const a = document.getElementById(element);
                 a.classList.remove('card');
             })
             cardsValue = [];
-
             cardsId = [];
         }
     } else if (e.target.className === 'card') {
