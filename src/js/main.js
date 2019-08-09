a2272338f5f5ab3dbc156e6df6cf6c1c213844f9
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-08-09 08:51:41 +0200

    feat: added html structure, basic styling and js from my test codepen

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,41 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
 
+const parent = document.querySelector('.parent');
+const array = [];
+let pairs = [];
+let counter = 0;
 
-console.log(`Hello world!`);
 
+parent.addEventListener('click', (e) => {
+    counter++;
+    if (counter <= 2) {
+        let id = parseInt(e.toElement.id);
+        const card = document.getElementById(id);
+        card.classList.add('card--reverse');
+        array.push(id);
+        pairs.push(card.innerHTML);
+        if (pairs[0] === pairs[1]) { alert('PARA'); }
+    } else {
 
+        counter = 0;
+        pairs = [];
+        array.forEach(element => {
+            const a = document.getElementById(element);
+            a.classList.remove('card--reverse');
+        })
+    }
+});
