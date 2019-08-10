66f0924f1c389bccdf428a7f80d3908f9365e531
Mateusz Kornecki <mateusz.kornecki@gmail.com>
2019-08-10 18:55:37 +0200

    feat: refactored code

--- src/js/main.js
+++ src/js/main.js
@@ -2,91 +2,121 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//             // Registration was successful
+//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//         }, function(err) {
+//             // registration failed :(
+//             console.log('ServiceWorker registration failed: ', err);
+//         });
+//     });
+// }
 
 // place your code below
+const cards = [];
+const discoveredCards = [];
 const parent = document.querySelector('.parent');
-let discoveredCards = [];
-let cardsValue = [];
-const displayPairCounter = document.querySelector('.pair-counter');
-let pairCounter = 0;
 
-//! Using Fisher-Yates Algorithm to shuffle an array >> got it from https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
-const shuffle = (array) => {
+const memo = {
+    shuffle: (array) => {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * array.length)
             const temp = array[i]
             array[i] = array[j]
             array[j] = temp
         }
-    }
-    // Create 
-const createCards = (value) => {
-    const array = [];
-    for (let i = 0; i < (value); i++) {
-        array.push(i);
-    }
-    shuffle(array);
-    for (let i = 0; i < value; i++) {
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.id = array[i];
-        //! GODMODE - comment code below to disable it
-        card.innerHTML = array[i];
-        parent.appendChild(card);
-    }
-}
+    },
 
-const game = () => {
-    parent.addEventListener('click', (e) => {
-        if (discoveredCards.length < 2 && e.target.className === 'card') {
-            let id = parseInt(e.toElement.id);
-            const card = document.getElementById(id);
-            card.classList.add('card-reverse', `card-reverse--${id}`);
-            discoveredCards.push(id);
-            let card1 = document.getElementById(discoveredCards[0]);
-            let card2 = document.getElementById(discoveredCards[1]);
-
-
-            if (discoveredCards.length > 1) {
-                let card1BackgroundColor = window.getComputedStyle(card1).getPropertyValue('background-color');
-                let card2BackgroundColor = window.getComputedStyle(card2).getPropertyValue('background-color');
-                console.log(card1BackgroundColor);
-                console.log(card2BackgroundColor);
-                // After finding pair
-                if (card1BackgroundColor === card2BackgroundColor) {
-                    pairCounter++;
-                    displayPairCounter.innerHTML = `Znaleziono ${pairCounter} par!`
-                    discoveredCards.forEach(element => {
-                        const a = document.getElementById(element);
-                        a.classList.remove('card');
-                        discoveredCards = [];
-                    })
-                } else {
-                    setTimeout(function() {
-                        discoveredCards.forEach(card => {
-                            const a = document.getElementById(card);
-                            a.classList.remove('card-reverse', `card-reverse--${card}`);
-                        })
-                        discoveredCards = [];
-                        cardsValue = [];
-
-                    }, 1500);
-
-                }
-            }
+    setAmount: (value) => {
+        for (let i = 0; i < value; i++) {
+            this.cards.push(i);
         }
-    });
-}
-createCards(16);
-game();
+        shuffle(cards);
+    },
+    createCards: () => {
+        this.cards.forEach((e, id) => {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.id = this.cards[id];
+            parent.appendChild(card);
+        });
+    }
+};
+
+// const parent = document.querySelector('.parent');
+// let discoveredCards = [];
+// let cardsValue = [];
+// const displayPairCounter = document.querySelector('.pair-counter');
+// let pairCounter = 0;
+
+//! Using Fisher-Yates Algorithm to shuffle an array >> got it from https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+// const shuffle = (array) => {
+//         for (let i = array.length - 1; i > 0; i--) {
+//             const j = Math.floor(Math.random() * array.length)
+//             const temp = array[i]
+//             array[i] = array[j]
+//             array[j] = temp
+//         }
+//     }
+//     // Create 
+// const createCards = (value) => {
+//     const array = [];
+//     for (let i = 0; i < (value); i++) {
+//         array.push(i);
+//     }
+//     shuffle(array);
+//     for (let i = 0; i < value; i++) {
+//         const card = document.createElement('div');
+//         card.className = 'card';
+//         card.id = array[i];
+//         parent.appendChild(card);
+//         // GODMODE - comment code below to disable it
+//         // card.innerHTML = array[i];
+//     }
+// }
+
+// const game = () => {
+//         parent.addEventListener('click', (e) => {
+//             if (discoveredCards.length < 2 && e.target.className === 'card') {
+//                 let id = parseInt(e.toElement.id);
+//                 const card = document.getElementById(id);
+//                 card.classList.add('card-reverse', `card-reverse--${id}`);
+//                 discoveredCards.push(id);
+//                 let card1 = document.getElementById(discoveredCards[0]);
+//                 let card2 = document.getElementById(discoveredCards[1]);
+
+
+//                 if (discoveredCards.length > 1) {
+//                     let card1BackgroundColor = window.getComputedStyle(card1).getPropertyValue('background-color');
+//                     let card2BackgroundColor = window.getComputedStyle(card2).getPropertyValue('background-color');
+//                     console.log(card1BackgroundColor);
+//                     console.log(card2BackgroundColor);
+//                     // After finding pair
+//                     if (card1BackgroundColor === card2BackgroundColor) {
+//                         pairCounter++;
+//                         displayPairCounter.innerHTML = `Znaleziono ${pairCounter} par!`
+//                         discoveredCards.forEach(element => {
+//                             const a = document.getElementById(element);
+//                             a.classList.remove('card');
+//                             discoveredCards = [];
+//                         })
+//                     } else {
+//                         setTimeout(function() {
+//                             discoveredCards.forEach(card => {
+//                                 const a = document.getElementById(card);
+//                                 a.classList.remove('card-reverse', `card-reverse--${card}`);
+//                             })
+//                             discoveredCards = [];
+//                             cardsValue = [];
+
+//                         }, 1500);
+
+//                     }
+//                 }
+//             }
+//         });
+//     }
+// createCards(12);
+// game();
